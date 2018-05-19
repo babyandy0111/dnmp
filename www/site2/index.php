2743b5c39bb1ed5a361a931244ba7933da86a8db
andy <babyandy0111@gmail.com>
2018-05-19 18:19:16 +0800

    add composer

--- www/site2/index.php
+++ www/site2/index.php
@@ -1,3 +1,17 @@
 <?php
-echo 'Site 2<br />';
+
+$link = mysqli_connect("mysql", "root", "123456");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+echo 'Site 2 eeeee<br />';
 
