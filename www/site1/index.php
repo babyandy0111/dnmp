2320f5cdc9ae252f90cd84211f826022bc3f0ec8
andy <babyandy0111@gmail.com>
2018-07-13 14:16:49 +0800

    fix

--- www/site1/index.php
+++ www/site1/index.php
@@ -7,18 +7,19 @@ if (!$link) {
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
+} else {
+  echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+  echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+  mysqli_close($link);
 }
 
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+$conn = new MongoDB\Driver\Manager("mongodb://mongo:27017");
 
-
-//$conn = new Mongo('mongo');
-$conn = new MongoClient();
 if (!$conn) {
     echo "Error: Unable to connect to mongo." . PHP_EOL;
     exit;
+} else {
+   echo "Success: A proper connection to MongoDB was made!" . PHP_EOL;
 }
 
-mysqli_close($link);
 phpinfo();
